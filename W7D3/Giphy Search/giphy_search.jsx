4de2b62770d7d15629039b35e412d751c497a896
Arvind Ravi <ar2au@virginia.edu>
2017-07-12 09:35:57 -0700

    Finished React-Redux Giphy Search project

--- W7D3/Giphy Search/giphy_search.jsx
+++ W7D3/Giphy Search/giphy_search.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-import { fetchSearchGiphys } from './util/api_util';
+//import { fetchSearchGiphys } from './actions/giphy_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
-  window.fetchSearchGiphys = fetchSearchGiphys;
+	const store = configureStore();
+  //window.store = store;
+  //window.fetchSearchGiphys = fetchSearchGiphys;
+	const root = document.getElementById('root');
+	ReactDOM.render(<Root store={store} />, root);
 });
