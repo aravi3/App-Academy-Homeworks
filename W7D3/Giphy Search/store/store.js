b1c863374f18358e0eb7ca0adc0dbe573452a069
Arvind Ravi <ar2au@virginia.edu>
2017-07-12 21:46:32 -0700

    React Routing

--- W7D3/Giphy Search/store/store.js
+++ W7D3/Giphy Search/store/store.js
@@ -3,7 +3,7 @@ import thunk from 'redux-thunk';
 import RootReducer from '../reducers/root_reducer';
 
 const configureStore = () => {
-  return createStore(RootReducer, applyMiddleware(thunk));
+  return createStore(RootReducer, {}, applyMiddleware(thunk));
 };
 
 export default configureStore;
